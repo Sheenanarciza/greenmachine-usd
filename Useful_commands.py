1810dec98114c314e8994f264b4f601b5ef0b6eb
sheenanarciza <sheenanarciza@gmail.com>
2023-05-13 04:19:14 +0000

    useful commands

---
+++ Useful_commands.py
@@ -0,0 +1,25 @@
+import random
+var = 2
+
+print(type(var))
+
+var2 = "str"
+
+print(type(var))
+#print will tell us it is a string
+
+print(dir(var))
+
+#output with _ are hidden ignore
+
+print(var2.capitalize())
+#this will return catpial S in Str
+
+
+var3 = "9"
+print(var3.zfill(3))
+
+
+number = random.randint(0,100)
+
+print(number)
